9de642030243ba335b86e6ec2619bd94bbaae61c
Shynih <jorgepeng2001@gmail.com>
2020-12-07 13:35:16 -0800

    fixed attack_test.hpp(1)

--- Tests/attack_test.hpp
+++ Tests/attack_test.hpp
@@ -11,7 +11,9 @@
 
 TEST(AttackTEST, EvsF) {
   Guy* user = new ElectricGuy();
+  user -> set_moves();
   Guy* opponent = new FireGuy();
+  opponent -> set_moves();
   Attack* newattack = new TypedAttackStrategy(user);
   user -> set_attack(newattack);
   user -> attack(opponent);
@@ -20,7 +22,9 @@ TEST(AttackTEST, EvsF) {
 
 TEST(AttackTEST, EvsW) {
   Guy* user = new ElectricGuy();
+  user -> set_moves();
   Guy* opponent = new WaterGuy();
+  opponent -> set_moves();
   Attack* newattack = new TypedAttackStrategy(user);
   user -> set_attack(newattack);
   user -> attack(opponent);
@@ -29,7 +33,9 @@ TEST(AttackTEST, EvsW) {
 
 TEST(AttackTEST, FvsE) {
   Guy* opponent = new ElectricGuy();
+  opponent -> set_moves();
   Guy* user = new FireGuy();
+  user -> set_moves();
   Attack* newattack = new TypedAttackStrategy(user);
   user -> set_attack(newattack);
   user -> attack(opponent);
@@ -38,7 +44,9 @@ TEST(AttackTEST, FvsE) {
 
 TEST(AttackTEST, GvsE) {
   Guy* user = new GrassGuy();
+  user -> set_moves();
   Guy* opponent = new ElectricGuy();
+  opponent -> set_moves();
   Attack* newattack = new TypedAttackStrategy(user);
   user -> set_attack(newattack);
   user -> attack(opponent);
@@ -47,7 +55,9 @@ TEST(AttackTEST, GvsE) {
 
 TEST(AttackTEST, FvsG) {
   Guy* user = new FireGuy();
+  user -> set_moves();
   Guy* opponent = new GrassGuy();
+  opponent -> set_moves();
   Attack* newattack = new TypedAttackStrategy(user);
   user -> set_attack(newattack);
   user -> attack(opponent);
@@ -56,7 +66,9 @@ TEST(AttackTEST, FvsG) {
 
 TEST(AttackTEST, WvsF) {
   Guy* user = new WaterGuy();
+  user -> set_moves();
   Guy* opponent = new FireGuy();
+  opponent -> set_moves();
   Attack* newattack = new TypedAttackStrategy(user);
   user -> set_attack(newattack);
   user -> attack(opponent);
@@ -65,7 +77,9 @@ TEST(AttackTEST, WvsF) {
 
 TEST(AttackTEST, NormalAttack ) {
   Guy* opponent = new GrassGuy();
+  opponent -> set_moves();
   Guy* user = new FireGuy();
+  user -> set_moves();
   Attack* newattack = new NormalAttackStrategy(user);
   user -> set_attack(newattack);
   user -> attack(opponent);
